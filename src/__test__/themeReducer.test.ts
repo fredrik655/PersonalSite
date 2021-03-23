73011f3dea29f41002acb8575b6f223e14242425
anon <anon@example.com>
2021-03-23 13:54:00 +0100

    Reducer and test for toggling between themes.

---
+++ src/__test__/themeReducer.test.ts
@@ -0,0 +1,14 @@
+import themeReducer from '../reducers/themeReducer';
+
+
+describe('test reducer actions', () => {
+  test('state should change between true and false when toggled', () => {
+    const initState = true;
+    const action = {type: 'theme/toggle'};
+
+    let result = themeReducer(initState, action);
+    expect(result).toBe(false);
+    result = themeReducer(result, action);
+    expect(result).toBe(true);
+  });
+});
