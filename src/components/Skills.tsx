b2d8e4d761e24e838087ae4e92e43f55a5cb4739
anon <anon@example.com>
2021-03-25 12:15:26 +0100

    Restructure component.

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,83 +1,111 @@
 import React from 'react';
+import htmlLogo from '../images/html5.svg';
+import cssLogo from '../images/css.svg';
+import javascriptLogo from '../images/javascript.svg';
+import sassLogo from '../images/sass.svg';
+import typescriptLogo from '../images/ts-logo-128.svg';
+import bootstrapLogo from '../images/bootstrap.svg';
+import reactLogo from '../images/react.svg';
+import reduxLogo from '../images/redux.svg';
+import jestLogo from '../images/jest.svg';
+import webpackLogo from '../images/webpack.svg';
+import gitLogo from '../images/git.svg';
+import githubLogo from '../images/github.svg';
+// import { useSelector } from 'react-redux';
 
 const Skills: React.FC = () =>  {
+  // const state = useSelector(state => state);
+
   return (
-    <div className="row mx-0">
-      <div className="card col-sm px-0 mx-3">
-        <div className="card-header text-white bg-dark">
-          Frontend Languages
-        </div>
-        <ul className="list-group-flush px-0 my-0">
-          <li className="list-group-item" id="ls-html">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            HTML
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Javascript
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Css
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Scss
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Typescript
-          </li>
-        </ul>
+    <div className="row mx-0 container-fluid g-0">
+      <div className="row text-sm-start text-center g-0">
+        <h2 className="mx-3">Skills</h2>
       </div>
-      <div className="card col-sm px-0 mx-3 ">
-        <div className="card-header text-white bg-dark">
-          Frameworks / Libraries
+      <div className="row container-fluid text-center g-0">
+        <div className="row text-sm-start text-center g-0">
+          <h3 className="mx-3">Front-end</h3>
         </div>
-        <ul className="list-group">
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Bootstrap
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            React
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Redux
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Jest
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Cypress
-          </li>
-        </ul>
-      </div>
-      <div className="card col-lg px-0 mx-3 ">
-        <div className="card-header text-white bg-dark">
-          Misc
+        <div className="row g-0">
+          <div className="col d-flex flex-row justify-content-md-start justify-content-center flex-wrap ">
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={htmlLogo} alt=""/>
+              <p className="fs-6">HTML</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={javascriptLogo} alt=""/>
+              <p>Javascript</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={cssLogo} alt=""/>
+              <p>Css</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={sassLogo} alt=""/>
+              <p>Sass</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={typescriptLogo} alt=""/>
+              <p>Typescript</p>
+            </div>
+          </div>
         </div>
-        <ul className="list-group">
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Webpack
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Git
-          </li>
-          <li className="list-group-item">
-            <img src="https://via.placeholder.com/25" alt=""/>
-            Github / Github Actions
-          </li>
-        </ul>
+        <div className="row text-sm-start text-center g-0">
+          <h3 className="mx-3">Frameworks & libraries</h3>
+        </div>
+        <div className="row g-0">
+          <div className="col d-flex flex-row justify-content-md-start justify-content-center flex-wrap">
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={bootstrapLogo} alt=""/>
+              <p>Bootstrap</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={reactLogo} alt=""/>
+              <p>React</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={reduxLogo} alt=""/>
+              <p>Redux</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={jestLogo} alt=""/>
+              <p>Jest</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={javascriptLogo} alt=""/>
+              <p>Cypress</p>
+            </div>
+          </div>
+          <div className="row text-sm-start text-center g-0">
+          <h3 className="mx-3">Misc</h3>
+        </div>
+        <div className="row g-0">
+          <div className="col d-flex flex-row justify-content-md-start justify-content-center flex-wrap">
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={webpackLogo} alt=""/>
+              <p>Webpack</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={gitLogo} alt=""/>
+              <p>Git</p>
+            </div>
+            <div className="rounded bg-light m-3 border-dark border" style={{maxWidth: "150px"}}>
+              <img className="m-2 p-2" style={{width: "100px"}} src={githubLogo} alt=""/>
+              <p>Github/Github Actions</p>
+            </div>
+          </div>
+        </div>
+          
+        </div>
       </div>
+      
+      
     </div>
   );
 }
 
 export default Skills;
+
+
+// html, javascript, css, sass, typescript
+// bootstrap, react, redux, jest, cypress
+// webpack, git, github/github actions
