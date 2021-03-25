ec94203eb72f9d747c6d5a22784d0cfcd92326d9
anon <anon@example.com>
2021-03-25 12:12:15 +0100

    fix svg import as module.

--- webpack.config.ts
+++ webpack.config.ts
@@ -33,6 +33,17 @@ const webpackConfig = (env): Configuration => ({
           "css-loader",
           "sass-loader",
         ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          }
+        ]
       }
     ]
   },
