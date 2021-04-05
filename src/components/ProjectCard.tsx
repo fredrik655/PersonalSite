98f64fa2bd9527a1b8a12d6e3c517c280622bc8e
anon <anon@example.com>
2021-04-05 17:12:02 +0200

    Projects and projects card components done.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import battleshipimg from '../images/battleship.png';
+import ResumeSiteImg from '../images/ResumeSite.png';
+import newsweekImg from '../images/newsweek.png';
+
 
 interface propsInterface {
   data: {
     title: string;
     text: string;
     tags: string[];
+    githubLink: string;
+    siteLink: string;
+    imgNumb?: number;
   }
 }
 
 const ProjectCard: React.FC<propsInterface> = (props: propsInterface) => {
+  const toggle = useSelector(state => state);
+  const loadImg = (numb) => {
+    if(numb === 1) {
+      return ResumeSiteImg;
+    }
+    else if(numb === 2) {
+      return newsweekImg;
+    }
+    else {
+      return battleshipimg;
+    }
+  }
   return (
-    <div className="card my-2">
-      <div className="row g-0">
-        <div className="col-md-4 p-2">
-          <img className="card-img" src="https://via.placeholder.com/1000" alt=""/>
+    <div className={`card my-2 ${toggle ? 'bg-light text-dark border-dark' : 'bg-dark text-light border-light' }`}>
+      <div className="row g-0 p-2">
+        <div className="col-lg p-2">
+          <img className="card-img border rounded border-dark" src={loadImg(props.data.imgNumb)} alt=""/>
         </div>
-        <div className="col-md-8">
+        <div className="col-lg">
           <div className="card-body">
             <div className="card-title">
               <h3>{props.data.title}</h3>
@@ -23,10 +43,13 @@ const ProjectCard: React.FC<propsInterface> = (props: propsInterface) => {
             <div className="card-text">
               {props.data.text}
             </div>
-            <div className="card-text">
-              {props.data.tags.map((tag, i )=> <p key={i} className="border rounded border-dark">{tag}</p>)}
+            <div className="card-text d-flex flex-wrap mt-2">
+              {props.data.tags.map((tag, i )=> <p key={i} className="border rounded border-dark me-2 p-2 bg-white text-dark tag-text">{tag}</p>)}
             </div>
-            
+            <div className="card-text">
+              <a href={props.data.githubLink}>Github</a>
+              <a  className="mx-2" href={props.data.siteLink}>Site</a>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
+import { useSelector} from 'react-redux';
 
-const data = {
-  title: 'test',
-  text: 'test text',
-  tags: ['react', 'redux']
-}
+
+const data = [{
+    title: 'Resume Site (this site)',
+    text: 'Web site for displaying all my projects.',
+    tags: ['react', 'redux', 'bootstrap', 'typescript', 'jest', 'cypress', 'scss',' github actions', 'webpack'],
+    githubLink: 'https://github.com/fredrik655/PersonalSite',
+    siteLink: 'https://fredrik655.github.io/PersonalSite',
+    imgNumb: 1
+  },
+  {
+    title: 'Using bootstrap',
+    text: 'Cloning Newsweek with the aim to practice using bootstrap',
+    tags: ['html', 'css', 'bootstrap'],
+    githubLink: 'https://github.com/fredrik655/usingbootstrap',
+    siteLink: 'https://fredrik655.github.io/usingbootstrap',
+    imgNumb: 2
+  },
+  {
+    title: 'Battleship',
+    text: 'Creating the game Battleship in javascript and React',
+    tags: ['html', 'css', 'javascript', 'react'],
+    githubLink: 'https://github.com/fredrik655/battleship',
+    siteLink: 'https://fredrik655.github.io/battleship/',
+    imgNumb: 3
+  },
+]
 
 const Projects: React.FC = () => {
+  const toggle = useSelector(state => state);
+
   return (
-    <div className="row container-fluid g-0 px-2">
-      <h2 className="mx-3 mt-3">Projects</h2>
-      <ProjectCard data={data}/>
-      <ProjectCard data={data}/>
-      <ProjectCard data={data}/>
+    <div className={`row container-fluid g-0 px-2 pt-5 ${toggle ? 'bg-light text-dark' : 'bg-dark text-light' }`} id="projects-target">
+      <h2 className="mx-3 mt-3 col">Projects</h2>
+      <ProjectCard data={data[0]}/>
+      <ProjectCard data={data[1]}/>
+      <ProjectCard data={data[2]}/>
     </div>
   );
 }
