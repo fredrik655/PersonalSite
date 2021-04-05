7b9d981e394b175a3b5847202bd61c60afea3708
anon <anon@example.com>
2021-04-05 17:19:54 +0200

    git projects filter styled.

--- src/components/GitProjectsFilter.tsx
+++ src/components/GitProjectsFilter.tsx
@@ -3,22 +3,22 @@ import {propFuncInterface} from './types';
 
 const GitProjectsFilter: React.FC<propFuncInterface> = (props:propFuncInterface) => {
   return (
-    <div className="row">
-      <div className="col">
-        <h3>Github Projects</h3>
+    <div className="row py-4 mx-0 justify-content-center" id="git-target">
+      <div className="col text-center text-sm-end">
+        <h3><strong>Github Projects</strong></h3>
       </div>
-      <div className="col">
+      <div className="col text-center text-sm-start">
         <div className="dropdown">
           <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
             Sort by
           </button>
           <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li onClick={props.func}><p data-FilterId={1} className="dropdown-item">Name</p></li>
-            <li onClick={props.func}><p data-FilterId={2} className="dropdown-item">Name reverse</p></li>
-            <li onClick={props.func}><p data-FilterId={3} className="dropdown-item">Date created oldest</p></li>
-            <li onClick={props.func}><p data-FilterId={4} className="dropdown-item">Date created newest</p></li>
-            <li onClick={props.func}><p data-FilterId={5} className="dropdown-item">last updated oldest</p></li>
-            <li onClick={props.func}><p data-FilterId={6} className="dropdown-item">last updated newest</p></li>
+            <li onClick={props.func}><p data-filterid={1} className="dropdown-item">Name</p></li>
+            <li onClick={props.func}><p data-filterid={2} className="dropdown-item">Name reverse</p></li>
+            <li onClick={props.func}><p data-filterid={3} className="dropdown-item">Date created oldest</p></li>
+            <li onClick={props.func}><p data-filterid={4} className="dropdown-item">Date created newest</p></li>
+            <li onClick={props.func}><p data-filterid={5} className="dropdown-item">Last updated oldest</p></li>
+            <li onClick={props.func}><p data-filterid={6} className="dropdown-item">Last updated newest</p></li>
           </ul>
         </div>
       </div>
